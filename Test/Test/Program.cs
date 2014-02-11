c0d6f5eb05ce4f0bcbd6741c538f5594176bcaf1
karashak1 <karashak1@hawkmail.newpaltz.edu>
2014-02-10 22:23:07 -0500

    started building a news gatherer

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
 
 namespace Test
 {
@@ -7,6 +10,18 @@ namespace Test
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+			string url = "http://news.google.com/?q=Disney&output=rss";
+			XmlReader reader = XmlReader.Create (url);
+			SyndicationFeed feed = SyndicationFeed.Load (reader);
+			foreach (SyndicationItem item in feed.Items) {
+				string subject = item.Title.Text;
+				string summery = item.Summary.Text;
+				Console.WriteLine (subject);
+				foreach (SyndicationLink link in item.Links) {
+					Console.WriteLine (link.Uri.ToString());
+				}
+				Console.WriteLine ();
+			}
 		}
 	}
 }
